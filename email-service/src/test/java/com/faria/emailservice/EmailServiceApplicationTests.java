ea2d0c711e48a3a2d12514cfb665bdd64ee853ad
João Pedro Faria <joaopedropepe56@gmail.com>
2023-12-07 09:53:20 -0300

    First commit
    First commit

---
+++ email-service/src/test/java/com/faria/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faria.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
